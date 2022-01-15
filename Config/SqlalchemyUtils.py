d36e75e568084f72c79386f153ed826140ccf13a
Seyyed Ali shohadaalhosseini <Al.shohadaee@gmail.com>
2022-01-15 17:17:27 +0330

    Directory Updated

--- Config/SqlalchemyUtils.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-engine = create_engine('sqlite:///D:\\My Works\\Projects\\Python Projects\\Project StackOverFlow\\StackoverfolwDB.db', echo=True)
-Base = declarative_base(bind=engine)
-Session = sessionmaker(bind=engine)
-session = Session()
--- Main.py
+++ Main.py
@@ -1,12 +1,34 @@
-from Config.SqlalchemyUtils import Base, engine
-from Model.TO.Answer import Answer
-from Model.TO.Comment import Comment
-from Model.TO.Question import Question
-from Model.TO.User import User
+import sys
+from PyQt5 import QtWidgets
+# from PyQt5.QtCore import Qt
+# from PyQt5.QtWidgets import QDialog, QApplication, QInputDialog, QListWidgetItem, QMessageBox, QPushButton
 
-user = User()
-question = Question()
-answer = Answer()
-comment = Comment()
+# Import UIs
+import Client.View.Login_Page
+import Client.View.Register_Page
 
-Base.metadata.create_all(engine, checkfirst=True)
+
+# ---------------------------------------- Main Code Start ---------------------------------------- #
+
+class Main:
+    def __init__(self):
+        self.register_page = Client.View.Register_Page.RegisterPage()
+        self.login_page = Client.View.Login_Page.LoginPage()
+        self.login_page.show()
+
+        self.login_page.RegisterButton.clicked.connect(self.show_register_page)
+
+    def show_register_page(self):
+        self.login_page.hide()
+        self.register_page.show()
+
+    def __str__(self):
+        pass
+
+
+# ---------------------------------------- Main Code End   ---------------------------------------- #
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = Main()
+    sys.exit(app.exec_())
