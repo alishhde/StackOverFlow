9111cc92be9ba6ecab3201659dbb1da3a5f827fb
Seyyed Ali shohadaalhosseini <Al.shohadaee@gmail.com>
2022-01-27 09:00:39 +0330

    Delete Server/Config directory

--- Server/Config/SqlalchemyUtils.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-engine = create_engine('sqlite:///E:\\Network Project\\Stackoverflow\\Server\\StackoverfolwDB.db', echo=True)
-Base = declarative_base(bind=engine)
-Session = sessionmaker(bind=engine)
-session = Session()
